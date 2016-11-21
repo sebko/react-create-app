b95eeaf1d88952fbd8ca6281446b5b9b28e28131
Sebastian Key <sebastianwkey@gmail.com>
2016-11-21 12:36:23 +1100

    TEAM-4688: added reporting for stylelint

---
+++ scripts/lint.js
@@ -0,0 +1,15 @@
+const gulpStylelint = require('gulp-stylelint');
+const stylelintCheckstyleFormatter = require('stylelint-checkstyle-formatter');
+const gulp = require('gulp');
+
+gulp
+  .src('src/**/*.css')
+  .pipe(gulpStylelint({
+    failAfterError: false,
+    reportOutputDir: 'reports/lint',
+    reporters: [
+      {formatter: 'verbose', console: true},
+      {formatter: stylelintCheckstyleFormatter, save: 'report/report.xml'}
+    ],
+    debug: true
+  }));
